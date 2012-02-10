b7ebb166c8e8fb3b1d01a084ca34adb25c88b76d
John Costa <john.costa@gmail.com>
2012-02-10 16:13:48 -0500

    add channel.html file

--- like_button/urls.py
+++ like_button/urls.py
@@ -1,1 +1,6 @@
-# no urls needed
+from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
+
+urlpatterns = patterns('',
+    url(r'^channel.html$', direct_to_template, {'template': 'channel.html'}),
+)
