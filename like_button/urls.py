e51d3d0a03f657fc1bad5ee523892b6b1053674b
John Costa <john.costa@gmail.com>
2013-09-28 06:54:12 -0400

    pep8

--- like_button/urls.py
+++ like_button/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import *
 from django.views.generic import TemplateView
 
-urlpatterns = patterns('',
-    url(r'^channel.html$', TemplateView.as_view(), {'template': 'channel.html'}),
+urlpatterns = patterns(
+    '',
+    url(r'^channel.html$', TemplateView.as_view(), {
+        'template': 'channel.html'}),
 )
